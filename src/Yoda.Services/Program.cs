6664f34ef405bdec60fd76d4a24ae8b486b8948d
NatOb <nat@ookbee.com>
2022-06-07 15:36:51 +0700

    allow cors

--- src/Yoda.Services/Program.cs
+++ src/Yoda.Services/Program.cs
@@ -1,9 +1,24 @@
 using Yoda.Services.Services.Authentication;
 using Yoda.Services.Services.User;
 
+
 var builder = WebApplication.CreateBuilder(args);
+var allowedHosts = builder.Configuration["AllowedHosts"];
+var allowedLocalhostOrigins = "AllowedLocalhostOrigins";
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: allowedLocalhostOrigins,
+        policy =>
+        {
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+            policy.WithOrigins(allowedHosts);
+        }
+    );
+});
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<ILoginService, LoginService>();
 builder.Services.AddTransient<IUserService, UserService>();
@@ -15,6 +30,8 @@ if (!app.Environment.IsProduction())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(allowedLocalhostOrigins);
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
