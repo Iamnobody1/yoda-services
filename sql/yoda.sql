0051da9a3ed1920e91864d301a7fca8d4b13c802
Eikkew <Thammajade.kewq@mail.kmutt.ac.th>
2022-06-14 17:28:25 +0700

    add yoda sql database

---
+++ sql/yoda.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public."Users" (
+	"ID" varchar(36) NULL,
+	"Username" varchar(256) NULL,
+	"Password" varchar(12) NULL,
+	"DisplayName" varchar(10) NULL,
+	"Avatar" varchar(256) NULL
+    
+);
