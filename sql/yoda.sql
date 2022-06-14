01827f4aa8238bffc8e6e3740daaa068d7ba6c24
Best <nekoth082@gmail.com>
2022-06-14 17:11:39 +0700

    Update SQL

--- sql/yoda.sql
+++ sql/yoda.sql
@@ -1,7 +1,8 @@
 CREATE TABLE public."Users" (
-	"ID" varchar(36) NULL,
+	"ID" varchar(36) NOT NULL,
 	"Username" varchar(256) NULL,
 	"Password" varchar(12) NULL,
 	"DisplayName" varchar(10) NULL,
-	"Avatar" varchar(256) NULL
+	"Avatar" varchar(256) NULL,
+	CONSTRAINT "Users_PK" PRIMARY KEY ("ID")
 );
