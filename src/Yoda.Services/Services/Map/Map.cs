aecb6090517819fef0c6d09839cd2f43196304a1
Best <nekoth082@gmail.com>
2022-07-01 16:39:12 +0700

    add Map

---
+++ src/Yoda.Services/Services/Map/Map.cs
@@ -0,0 +1,11 @@
+using Yoda.Services.Models;
+
+namespace Yoda.Services.Services.Map
+{
+    public class Map : IMap
+    {
+        private readonly YodaContext _yodaContext;
+
+        public 
+    }
+}
